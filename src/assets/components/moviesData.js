c0b2abe81d23b9808a18cc6528927b21bb1e1c6e
piperiao <andresfeliperianom@gmail.com>
2024-08-11 21:26:02 -0500

    fix the movies URL

--- src/assets/components/moviesData.js
+++ src/assets/components/moviesData.js
@@ -1,6 +1,6 @@
 export const movies = [
     { src: "https://m.media-amazon.com/images/M/MV5BYTY0NTZlZjYtNWRmZi00MGRjLTk4ZDctMjE4NjBiZTUyNGNhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg", title: "Mártires", url: "https://drive.google.com/file/d/1UrZkjgApyfiUvYiMrRMyYVYDGNZgtNqb/preview" },
-    { src: "https://pics.filmaffinity.com/Ghostland-186804143-mmed.jpg", title: "Pesadilla en el infierno", url: "https://drive.google.com/file/d/1aJ7K7nkEGFURiIfLj82Rb7oSA_Mh_pB5/preview" },
+    { src: "https://pics.filmaffinity.com/Ghostland-186804143-mmed.jpg", title: "Pesadilla en el infierno", url: "https://drive.google.com/file/d/1B3KZBNcDRql7csa2i1L9uVqzKdmoGIvj/preview" },
     { src: "https://pics.filmaffinity.com/the_poughkeepsie_tapes-852900426-large.jpg", title: "las cintas de poughkeepsie", url: "https://drive.google.com/file/d/1cfWylScrd0aZmLZX92MC88-x7-Ot6ze6/preview" },
     { src: "https://m.media-amazon.com/images/S/pv-target-images/d1893dd7510bfba7585259259a4005cac499e935c1e6791684b1887a0d07fc1d.jpg", title: "La venganza de la casa del lago", url: "https://drive.google.com/file/d/1ChZZkuYMQHrrJMbyCm0p55z2tu0e6uUH/preview" },
     { src: "https://pics.filmaffinity.com/funny_games_u_s-831316674-large.jpg", title: "Juegos Sádicos", url: "https://drive.google.com/file/d/14E3xKo1QdW6lnFnxf9Uq9J0lFT9a5SRN/preview" },
@@ -17,7 +17,7 @@ export const movies = [
     { src: "https://m.media-amazon.com/images/S/pv-target-images/71bfb694a478414eff36ac9245d8a1c1baeb6418c01fc48506eefbca075e5a10.png", title: "Relic: Herencia maldita", url: "https://drive.google.com/file/d/1P55FqVF4Gb9wwJUBbPxbsNEQWqHpdSb7/preview" },
     { src: "https://m.media-amazon.com/images/S/pv-target-images/a25489f875a64d1a352130b85b5b3da1e1b1fcc71f105c0fa66cddb4d9f29c27.jpg", title: "El ciempiés humano", url: "https://drive.google.com/file/d/1bPJdP-XsEUdHb6mVnon-JptIRQm_f_3l/preview" },
     { src: "https://m.media-amazon.com/images/S/pv-target-images/482b96ad3ff30f475f92e7e54f05f821a768e10782705b5b4683a9a4d1007f84.jpg", title: "El ciempiés humano 2", url: "https://drive.google.com/file/d/1URy1nsyU5scS7FEa7YP15dw9jqMSBQZZ/preview" },
-    { src: "https://pics.filmaffinity.com/Pesadilla_en_Elm_Street-790429633-large.jpg", title: "Pesadilla en la calle Elm", url: "https://drive.google.com/file/d/1Ldje1UiT0ziFl3Zg6NeLgPnfy76z21hQ/preview" },
+    { src: "https://pics.filmaffinity.com/Pesadilla_en_Elm_Street-790429633-large.jpg", title: "Pesadilla en la calle Elm", url: "https://drive.google.com/file/d/1aJ7K7nkEGFURiIfLj82Rb7oSA_Mh_pB5/preview" },
     { src: "https://m.media-amazon.com/images/S/pv-target-images/4ad403a60cf5fbf5d47167fbdf923443c92940950aa606e29c9660d3a8a70998.jpg", title: "Pesadilla en la calle Elm 2", url: "https://drive.google.com/file/d/1venRiJm30u3PXaokrjdKpMT6VSs9C6FA/preview" },
     { src: "https://m.media-amazon.com/images/S/pv-target-images/058d164cead01821a52b2d8dd5f985a4f0599b9c3028378c3b1bda5afa83978b.jpg", title: "Pesadilla en la calle Elm 3", url: "https://drive.google.com/file/d/1X9cyBUMvRJ1-9rSAT--hsuMzsBAijPUJ/preview" },
     { src: "https://musicart.xboxlive.com/7/6b211100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080", title: "Pesadilla en la calle Elm 4", url: "https://drive.google.com/file/d/1Ldje1UiT0ziFl3Zg6NeLgPnfy76z21hQ/preview" },
