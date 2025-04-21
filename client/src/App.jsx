8880e42f7c6110799e4188de96dfe34319c450b0
afriano <andresfeliperianom@gmail.com>
2025-04-21 15:21:41 -0600

    testing chatbot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,12 @@ function App() {
       ) : (
         <>
         <Chatbot />
+        <iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/c626dcf6-489d-4ed1-a4d5-c5ca36f67c33">
+</iframe>
         </>
       )}
     </div>
